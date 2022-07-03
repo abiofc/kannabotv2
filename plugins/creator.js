4555892b28165746b23946582930aeff6226a214
abiofc <108221611+abiofc@users.noreply.github.com>
2022-07-03 19:32:55 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
+*ɴᴀᴍᴇ:* AbiCxy
 *ᴀɢᴇ:* 20ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
 *ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Tengah, Bandung
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
